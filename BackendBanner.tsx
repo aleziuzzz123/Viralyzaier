406e74e0b9c91170b88478e19c23ea07df21feb0
aleziuzzz123 <aichatsyapp@gmail.com>
2025-08-03 23:37:48 -0500

    Delete BackendBanner.tsx

--- BackendBanner.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// This is a placeholder component to prevent the application from crashing
-// due to an empty file being imported. It can be implemented later if needed.
-const BackendBanner: React.FC = () => {
-  return null; // Render nothing for now
-};
-
-export default BackendBanner;
