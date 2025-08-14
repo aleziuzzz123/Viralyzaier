24fa25fd23d8ac48d328c43928cd0f37ffd60d2c
aleziuzzz123 <aichatsyapp@gmail.com>
2025-08-14 14:15:06 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-    };
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
 });
