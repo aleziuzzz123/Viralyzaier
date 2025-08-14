4e1825a83394ba92ab8b30a1455c1551b4697534
aleziuzzz123 <aichatsyapp@gmail.com>
2025-08-14 14:50:26 -0500

    Add files via upload

--- api/cesdk-assets/[...path].ts
+++
@@ -1,3 +0,0 @@
-// This file is intentionally left without a default export to resolve a Vercel build conflict.
-// It can be safely deleted. All logic is handled by [[...path]].ts.
-export const message = "This file is deprecated.";
